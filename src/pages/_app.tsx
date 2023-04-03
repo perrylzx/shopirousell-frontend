66709f3ba561b88907cd8e8c95f7220c08caae33
Zhi Xian Lim <perry_lim_from.tp@tech.gov.sg>
2023-04-04 06:04:04 +0800

    Add color palettes and global styles in ts.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,18 @@
 import '@/styles/globals.css';
+import { colors } from '@/styles/globals';
+import { ConfigProvider } from 'antd';
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ConfigProvider
+      theme={{
+        token: {
+          colorPrimary: colors.base[2],
+        },
+      }}
+    >
+      <Component {...pageProps} />
+    </ConfigProvider>
+  );
 }
