68e8b31c46dbc20df3b371a707b7afcbe67d32de
Zhi Xian Lim <perry_lim_from.tp@tech.gov.sg>
2023-04-04 06:03:08 +0800

    Add additional eslint rules

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,18 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import Head from 'next/head';
+import Image from 'next/image';
+import { Inter } from 'next/font/google';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>{"Perry's Portfolio"}</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <title>Perry&apos;s Portfolio</title>
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <link rel='icon' href='/favicon.ico' />
       </Head>
-      <main>
-      </main>
+      <main></main>
     </>
-  )
+  );
 }
